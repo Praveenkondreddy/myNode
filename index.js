a473fce44a65435dea591374df9003ec6694909c
Praveen Kondreddy <praveennov26@gmail.com>
2022-06-29 13:40:47 +0530

    first commit

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const express=require("express")
-const mysql=require('mysql')
+var mysql = require('mysql');
 
 
-
-const db=mysql.createConnection({
+var pool  = mysql.createPool({
   host:"aws-sample-db.cko4fkw4kocs.ap-south-1.rds.amazonaws.com",
   user:"admin",
   password:"Praveen123",
   port:"3306",
   database:"sampledb"
+  });
 
-})
+exports.handler =  (event, context, callback) => {
+  //prevent timeout from waiting event loop
+  context.callbackWaitsForEmptyEventLoop = false;
+  pool.getConnection(function(err, connection) {
+    // Use the connection
+    connection.query('SELECT name from users where id=1', function (error, results, fields) {
+      // And done with the connection.
+      connection.release();
+      // Handle error after the release.
+      if (error) callback(error);
+      else callback(null,results[0].name);
 
-db.connect((err)=>{
-  if (err) throw err
-  console.log("connected")
-})
+    });
+  });
+};
 
-const app=express()
 
 
 
